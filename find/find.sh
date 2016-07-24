b23e127b4898c4f1b9c9d66ccd3da9dcf9418287
Pierre Fenoll <pierrefenoll@gmail.com>
2016-07-24 22:31:18 +0200

    Find Erlang Github repos with Google BigQuery for free

--- find/find.sh
+++ find/find.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Also this BigQuery
+#  SELECT repo_name FROM [bigquery-public-data:github_repos.languages] WHERE language.name = 'Erlang'
+# then cat results-*.csv | tail -n +2 | sed 's%^%github.com/%' | tr '[:upper:]' '[:lower:]' >seed.bq
+
 [[ "$(basename "$(dirname "$0")")" != 'find' ]] && echo cd into find/ && exit 2
 
 [[ $# -ne 1 ]] && echo "Usage: $0  website dir" && exit 1
