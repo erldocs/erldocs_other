131b94bff55d0fde5f53604028c24abdab9528f4
Pierre Fenoll <pierrefenoll@gmail.com>
2016-07-25 16:18:19 +0200

    fix find.sh

--- find/find.sh
+++ find/find.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-[[ "$(basename "$(dirname "$0")")" != 'find' ]] && echo cd into find/ && exit 2
+[[ "$(basename "$PWD")" != 'find' ]] && echo cd into find/ && exit 2
 
-[[ $# -ne 1 ]] && echo "Usage: $0  website dir" && exit 1
 ofile=$RANDOM
 touch $ofile
 
 finders="$(ls -1 ??.sh)"
 P=$(echo "$finders" | wc -l)
-echo "$finders" | xargs -P $P -n 1 -t
+echo "$finders" | xargs -P $P -n 1 -t -- bash
 
 sort -u seed.* > $ofile
 wc -l seed.* $ofile
