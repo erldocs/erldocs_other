82075da0a714feb4004965c0f2ad7c9d8d7ada17
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-12 20:10:39 +0200

    master: log cron job time

--- ocron.sh
+++ ocron.sh
@@ -8,7 +8,7 @@ osite=~/www/dev.erldocs.com
 wef=~/wefwefwef/docs
 code=$wef/erldocs_other.git
 urls=$wef/gen
-urls_log=${urls}.log
+log=${urls}.log
 tmp_urls=$RANDOM
 seeds=$code/seeds
 NPROCS=7
@@ -26,13 +26,13 @@ sort -u $seeds $urls >$tmp_urls
 mv $tmp_urls $urls
 
 pushd $code
-echo >$urls_log
+echo >$log
 #./gen.escript $osite $wef/other/ $urls 2>&1 | tee --append $wef/gen_log
 count=$(cat $urls | wc -l)
 echo $count
-#cat $urls | pv -pet -i 0.5 -l -s $count | (xargs -P $NPROCS -n 1 -t -- ./gen.escript $osite $wef/other/ >>$urls_log)
-##xargs -a $urls -P $NPROCS -I{} -t -- ./gen.escript $osite $wef/other/ "'"{}"'" >>$urls_log
-xargs -a $urls -P $NPROCS -n 1 -t -- ./gen.escript $osite $wef/other/ >>$urls_log
+#cat $urls | pv -pet -i 0.5 -l -s $count | (xargs -P $NPROCS -n 1 -t -- ./gen.escript $osite $wef/other/ >>$log)
+##xargs -a $urls -P $NPROCS -I{} -t -- ./gen.escript $osite $wef/other/ "'"{}"'" >>$log
+xargs -a $urls -P $NPROCS -n 1 -t -- ./gen.escript $osite $wef/other/ >>$log
 # ./pu.sh ~/wefwefwef/docs/osite.git/ 'daily update'
 popd
 
@@ -44,6 +44,6 @@ echo '];' >>$apps
 popd
 
 echo
-echo $S
-echo $(date)
+echo $S >>$log
+echo $(date) >>$log
 echo DONE
